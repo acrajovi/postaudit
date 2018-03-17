0074b0bf3b7db414b5115176af1b853cfef1135e
BIOST <BIOST@BIOST-ENVY>
2018-03-17 10:06:48 -0300

    cambios para prueba

--- src/service/BaseManager.java
+++ src/service/BaseManager.java
@@ -3,7 +3,6 @@
  *
  * Created on 10 de enero de 2012, 11:49 AM
  */
-
 package service;
 
 import java.sql.ResultSet;
@@ -22,25 +21,28 @@ import dao.impl.BaseDao;
  * @author sfernandez
  */
 public class BaseManager {
+
     protected Dao dao;
-    
+
     private ResultSet rs;
-    
+
     private Map<String, Object> record = null;
     private List<Map<String, Object>> result = null;
     private BeanUtil beanUtil = new BeanUtil();
-    
-    /** Creates a new instance of BaseManager */
+
+    /**
+     * Creates a new instance of BaseManager
+     */
     public BaseManager() {
         this.dao = new BaseDao();
     }
-    
+
     /**
-     * Obtiene todos los registros de una tabla en base a la definición
-     * del bean correspondiente.
+     * Obtiene todos los registros de una tabla en base a la definición del bean
+     * correspondiente.
      *
      * @param Class
-     * @param Object 
+     * @param Object
      */
 //    public BaseObject getRegistro(Class<? extends BaseObject> clazz, Object id) throws SQLException {
 //        BaseObject bObject = null;
@@ -63,28 +65,29 @@ public class BaseManager {
 //        
 //        return bObject;
 //    }
-
     /**
-     * Obtiene todos los registros de una tabla mapeada a un Bean, los carga en un List
-     * @param clazz
+     * Obtiene todos los registros de una tabla mapeada a un Bean, los carga en
+     * un List
+     * @autor:
+     * @param clazz clase
      * @param sql
      * @return List<Class<? extends BaseObject>>
      */
     public List getRegistros(Class<? extends BaseObject> clazz, String sql) {
         BaseObject bObject = null;
         List listaRegistro = new ArrayList();
-
+        //comentario de prueba
         try {
             List<Map<String, Object>> registros = beanUtil.prepareData(this.getDBDataAsRs(sql));
 
-            for ( Map<String, Object> registro : registros ) {
+            for (Map<String, Object> registro : registros) {
                 bObject = clazz.newInstance();
 
                 beanUtil.populateObject(bObject, registro);
 
                 listaRegistro.add(bObject);
             }
-            
+
         } catch (IllegalAccessException ex) {
             ex.printStackTrace();
         } catch (InstantiationException ex) {
@@ -95,12 +98,11 @@ public class BaseManager {
 
         return listaRegistro;
     }
-    
+
     /**
-     * Método útil para ejecutar un INSERT en base a un objeto que herede de 
-     * BaseObject.
-     * El String sql se arma en base a los annotations del Bean.
-     * Por el momento éste método es util para tablas con claves simples
+     * Método útil para ejecutar un INSERT en base a un objeto que herede de
+     * BaseObject. El String sql se arma en base a los annotations del Bean. Por
+     * el momento éste método es util para tablas con claves simples
      *
      * @param BaseObject
      * @return boolean
@@ -109,11 +111,10 @@ public class BaseManager {
     public boolean insert(BaseObject obj) throws SQLException {
         return dao.insert(obj);
     }
-    
+
     /**
-     * Método útil para ejecutar un DELETE en base a un objeto que herede de 
-     * BaseObject.
-     * El String sql se arma en base a los annotations del Bean.
+     * Método útil para ejecutar un DELETE en base a un objeto que herede de
+     * BaseObject. El String sql se arma en base a los annotations del Bean.
      *
      * @param BaseObject
      * @return boolean
@@ -122,12 +123,11 @@ public class BaseManager {
     public void delete(BaseObject obj) throws SQLException {
         dao.delete(obj);
     }
-    
+
     /**
-     * Método útil para ejecutar un UPDATE en base a un objeto que herede de 
-     * BaseObject.
-     * El String sql se arma en base a los annotations del Bean.
-     * Es importante que se obtenga el registro de la base de datos 
+     * Método útil para ejecutar un UPDATE en base a un objeto que herede de
+     * BaseObject. El String sql se arma en base a los annotations del Bean. Es
+     * importante que se obtenga el registro de la base de datos
      *
      * @param BaseObject
      * @throws SQLException
@@ -135,80 +135,77 @@ public class BaseManager {
     public void update(BaseObject obj) throws SQLException {
         dao.update(obj);
     }
-    
+
     /**
      * @param String
-     * @return List
-     * Devuelte un List de Map<String, Object>. La llave corresponde al nombre
-     * de la columna en la base de datos, y el valor es el valor correspondiente
-     * para esa columna para ese registro
+     * @return List Devuelte un List de Map<String, Object>. La llave
+     * corresponde al nombre de la columna en la base de datos, y el valor es el
+     * valor correspondiente para esa columna para ese registro
      */
     protected List<Map<String, Object>> getDBData(String sql) throws SQLException {
         result = new ArrayList<Map<String, Object>>();
         record = null;
         rs = null;
-        
+
         result = beanUtil.prepareData(dao.getDBData(sql));
-        
+
         return result;
     }
-    
+
     protected ResultSet getDBDataAsRs(String sql) throws SQLException {
         return dao.getDBData(sql);
     }
-    
+
     /**
      * @param String
      * @param List
-     * @return List
-     * Devuelte un List de Map<String, Object> con parámetros. 
-     * La lista de parámetros (params) debe tener los parámetros en orden
-     * según la consulta SQL.
-     * La llave corresponde al nombre de la columna en la base de datos, 
-     * y el valor es el valor correspondiente para esa columna para ese registro. 
+     * @return List Devuelte un List de Map<String, Object> con parámetros. La
+     * lista de parámetros (params) debe tener los parámetros en orden según la
+     * consulta SQL. La llave corresponde al nombre de la columna en la base de
+     * datos, y el valor es el valor correspondiente para esa columna para ese
+     * registro.
      */
     protected List<Map<String, Object>> getDBData(String sql, List params) throws SQLException {
         result = new ArrayList<Map<String, Object>>();
         record = null;
         rs = null;
-        
+
         result = beanUtil.prepareData(dao.getDBData(sql, params));
-        
+
         return result;
     }
-    
+
     protected ResultSet getDBDataAsRs(String sql, List params) throws SQLException {
         return dao.getDBData(sql, params);
     }
-    
+
     /**
      * @param String
-     * @return boolean
-     * Ejecuta un consulta SQL
+     * @return boolean Ejecuta un consulta SQL
      */
     protected boolean executeQuery(String sql) throws SQLException {
         return dao.executeQuery(sql);
     }
-    
+
     /**
      * @param String
      * @param List
-     * @return boolean
-     * Ejecuta una consulta SQL con parámetros.
-     * La lista de parámetros (params) debe tener los parámetros en orden
-     * según la consulta SQL
+     * @return boolean Ejecuta una consulta SQL con parámetros. La lista de
+     * parámetros (params) debe tener los parámetros en orden según la consulta
+     * SQL
      */
     protected boolean executeQuery(String sql, List params) throws SQLException {
         return dao.executeQuery(sql, params);
     }
-    
+
     private void closeResources() {
         try {
-            if ( rs != null )
+            if (rs != null) {
                 rs.close();
+            }
 
             dao.closeResources();
-        } catch ( SQLException ex ) {
+        } catch (SQLException ex) {
             System.err.println(">> Ocurrió un error cerrando los recursos");
             ex.printStackTrace();
         }
